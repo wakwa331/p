79fb18aaf281c5259b42c1c293243cc1055d82e4
wakwa331 <78485415+wakwa331@users.noreply.github.com>
2022-11-10 02:07:21 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://api.reysekha.xyz';
+var url = 'https://pinostore.my.id';
 try {
-if(url == 'https://api.reysekha.xyz') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://pinostore.my.id') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
